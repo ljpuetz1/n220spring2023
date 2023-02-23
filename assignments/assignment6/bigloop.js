909fff420a587a488e3853f4c8c7a4f39eaf17c5
ljpuetz1 <ljpuetz@iu.edu>
2023-02-23 11:28:31 -0500

    owvnpaonvanopwenfonmwr

---
+++ assignments/assignment6/bigloop.js
@@ -0,0 +1,17 @@
+
+//create global variable 
+let i=0
+// have the global variable count up because loop
+while (i<=4000){
+    console.log("yummycode yum yum I love eating code");
+   i= i+1000
+   let loop=document.createElement("div");
+loop.style.width=100;
+loop.style.height=100;
+loop.innerHTML=i
+document.body.appendChild(loop);
+
+}
+
+
+
