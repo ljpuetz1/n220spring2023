f8cf387ab15e0769ea12b748a2c99d79e4fcc2ce
ljpuetz1 <ljpuetz@iu.edu>
2023-04-06 11:44:30 -0400

    akngponw

--- assignments/assignment 9/average.js
+++ assignments/assignment 9/average.js
@@ -1,12 +1,29 @@
 var numbers=[];
 
 function run(){
-inputValue=document.getElementById('valNum').value
-numbers.push(inputValue);
+inputValue=document.getElementById('valNum').value;
+    let splitstring=inputValue.split(",");
+    numbers.push(splitstring);
+    
+    var total=0;
+    for(i=0;i<splitstring.length;i++){
+        var x= Number(splitstring[i]);
 
-inputValue.split(",")
+        total+=x;
 
-
-console.log(inputValue)
-console.log(numbers);
+    }
+    
+    console.log(total);
+    console.log(numbers);
 }
+
+/*
+console.log(numbers);
+
+let myArray=[1,2,3];
+var TOTAL = 0;
+for (i=0; i<3; i++){
+    TOTAL += myArray[i]
+    }
+    console.log(TOTAL);
+    */
