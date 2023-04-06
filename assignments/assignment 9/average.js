da852543f70334c8d30f1a515276b14a133d5c59
ljpuetz1 <ljpuetz@iu.edu>
2023-04-06 00:30:23 -0400

    wopfna

---
+++ assignments/assignment 9/average.js
@@ -0,0 +1,12 @@
+var numbers=[];
+
+function run(){
+inputValue=document.getElementById('valNum').value
+numbers.push(inputValue);
+
+inputValue.split(",")
+
+
+console.log(inputValue)
+console.log(numbers);
+}
