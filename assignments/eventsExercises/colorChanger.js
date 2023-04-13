a1ad18aae7cf00521a69b4415f412cf18600f430
ljpuetz1 <ljpuetz@iu.edu>
2023-04-13 11:51:45 -0400

    aldnvjknwal

---
+++ assignments/eventsExercises/colorChanger.js
@@ -0,0 +1,12 @@
+
+
+
+function changeColor(event){
+    let tell=event.target.getAttribute("data-response")
+    
+
+    event.target.style.backgroundColor=tell
+    console.log("yeah baby i got clicked baby baby yeah.")
+    console.log(tell)
+
+}
