687fca8c4d8fa1fd9bd25f688097c432f8987548
ljpuetz1 <ljpuetz@iu.edu>
2023-04-26 19:54:01 -0400

    aspiovnw

--- assignments/finalProject/ticTacToe.js
+++ assignments/finalProject/ticTacToe.js
@@ -1,11 +1,15 @@
+//create an array of numbers to track the ammount of times clicked. 
 var numberClick = [];
 for (var i = 0; i <= 50; i++) {
   numberClick.push(i);
 }
+//create some variables to place in the boxes on click 
 var oText = "O";
 var xText = "X";
+//these variables will correspond to the array for the even and odd clicks
 var xClick = numberClick.filter((x) => x % 2 === 0);
 var oClick = numberClick.filter((n) => n % 2);
+ 
 
 console.log(oClick);
 console.log(numberClick);
