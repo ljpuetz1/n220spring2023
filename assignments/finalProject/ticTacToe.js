b1309d5be58c803b068d0c98c404890be885a7b6
ljpuetz1 <ljpuetz@iu.edu>
2023-04-27 01:34:45 -0400

    ih

--- assignments/finalProject/ticTacToe.js
+++ assignments/finalProject/ticTacToe.js
@@ -1,16 +1,49 @@
-//create an array of numbers to track the ammount of times clicked. 
+//create an array of numbers to track the ammount of times clicked.
 var numberClick = [];
 for (var i = 0; i <= 50; i++) {
   numberClick.push(i);
 }
-//create some variables to place in the boxes on click 
-var oText = "O";
-var xText = "X";
 //these variables will correspond to the array for the even and odd clicks
 var xClick = numberClick.filter((x) => x % 2 === 0);
 var oClick = numberClick.filter((n) => n % 2);
- 
 
-console.log(oClick);
-console.log(numberClick);
-console.log(xClick);
+//create some variables to place in the boxes on click
+var oText = "O";
+var xText = "X";
+var cube = document.querySelectorAll(".cube");
+
+cube.forEach((cube) => {
+  cube.addEventListener("click", () => {
+    clickCount++;
+  });
+});
+
+//variable to grab the cube in order to have an event listener
+let clickCount = 0;
+function check(event) {
+  cube.forEach((cube) => {
+    cube.addEventListener("click", () => {
+      if (clickCount % 2 == 0) {
+        cube.innerHTML = xText;
+      } else {
+        cube.innerHTML = oText;
+      }
+    });
+  });
+  let tellMe = event.target.getAttribute("data-answer");
+  console.log(tellMe);
+  console.log(clickCount);
+}
+function winCon() {
+  for (let i = 0; i < 9; i + 3) {
+    if (
+      cube[i].innerHTML !== "" &&
+      cube[i].innerHTML === cube[i + 1].innerHTML &&
+      cube[i + 1].innerHTML === cube[i + 2].innerHTML
+    ) {
+      return true;
+    }
+  }
+}
+
+console.log(winCon);
